c2ccf9714e1d662c398827509c84be8ba7ddc4fe
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2022-01-25 14:56:52 +0700

    Update __init__.py
    y

--- lang/__init__.py
+++ lang/__init__.py
@@ -4,5 +4,5 @@ import json
 
 
 def load(lang):
-    return json.load(open(f"./lang/{lang}", "r"))
-    return json.load(open(f"./lang/{lang}", "r"))
+    return json.load(open(f"./lang/id", "r"))
+    return json.load(open(f"./lang/en", "r"))
